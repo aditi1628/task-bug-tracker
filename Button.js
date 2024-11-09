4055dc91789cea5998e706ddcd6c7e60d163979c
aditi1628 <118006771+aditi1628@users.noreply.github.com>
2024-11-09 13:38:29 +0530

    Delete Button.js

--- Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Button.css";
-const Button = ({ onClick, children, type = "button" }) => (
-  <button type={type} onClick={onClick} className="button" disabled={disabled}>
-    {children}
-  </button>
-);
-
-export default Button;
